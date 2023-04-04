1fec8bf12497420b877b341ba9f6cf6b538ed7a0
tashtamirov <haron-2000@mail.ru>
2023-04-04 12:07:28 +0300

    Сверстал header, main компонент

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Categories from "./components/categories/Categories";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Header />
+      <Main />
+      <Categories />
     </div>
   );
 }
---
+++ src/features/authSlice.js
@@ -0,0 +1,19 @@
+// const { createAsyncThunk } = require("@reduxjs/toolkit")
+
+// const initialState = {
+//     signUp: false,
+//     signIn: false,
+//     error: null,
+//     token: localstorage.getItem("token")
+// }
+
+// const authSignUp = createAsyncThunk(
+//     "auth/signup", 
+//     async ({login, password}) => {
+//         try {
+//             const res = await fetch('http://localhost:4000/auth/', {
+//                 method: "POST"
+//             })
+//         }
+//     }
+// )
